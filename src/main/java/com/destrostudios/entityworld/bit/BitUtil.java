e222a8ec08df67b3152bdde5cb4c0b94d3984463
carlkuesters <carl.kuesters@youengineering.com>
2022-08-13 20:45:46 +0200

    Add writeToBytes/readFromBytes util methods.

--- src/main/java/com/destrostudios/entityworld/bit/BitUtil.java
+++ src/main/java/com/destrostudios/entityworld/bit/BitUtil.java
@@ -1,8 +1,25 @@
 package com.destrostudios.entityworld.bit;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class BitUtil {
 
     public static int getNeededBitsCount(int value) {
         return (32 - Integer.numberOfLeadingZeros(value));
+    }
+
+    public static byte[] writeToBytes(BitSerializable bitSerializable){
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        BitOutputStream bitOutputStream = new BitOutputStream(byteArrayOutputStream);
+        bitSerializable.write(bitOutputStream);
+        bitOutputStream.close();
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    public static void readFromBytes(BitSerializable bitSerializable, byte[] bytes) throws IOException {
+        BitInputStream bitInputStream = new BitInputStream(new ByteArrayInputStream(bytes));
+        bitSerializable.read(bitInputStream);
     }
 }
